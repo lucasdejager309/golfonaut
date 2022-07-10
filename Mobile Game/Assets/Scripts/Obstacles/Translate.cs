d34633aae80c51d4ecce2ea840518ad88758c462
lucas <lucasdejager309@gmail.com>
2022-07-10 22:52:09 +0200

    added teleport functionality to translate script

--- Mobile Game/Assets/Scripts/Obstacles/Translate.cs
+++ Mobile Game/Assets/Scripts/Obstacles/Translate.cs
@@ -2,36 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TranslateMode {
+    DoReverse,
+    Teleport
+}
+
 public class Translate : MonoBehaviour
 {
     public float moveSpeed;
-    public Vector2 relativePos1;
-    public Vector2 relativePos2;
+    public TranslateMode mode;
+    public Vector2[] points;
 
     Vector2 origin;
 
+    int index = 0;
+
+    Task t;
+
     void Start() {
         origin = transform.position;
-        MoveToPos1();
-    }
-
-    void MoveToPos1() {
-        Task t = new Task(MoveTo(relativePos1, moveSpeed));
-        t.Finished += delegate {MoveToPos2();};
-    }
-
-    void MoveToPos2() {
-        Task t = new Task(MoveTo(relativePos2, moveSpeed));
-        t.Finished += delegate {MoveToPos1();};
+        transform.position = origin-points[0];
+        t = new Task(MoveTo(points[index], moveSpeed));
     }
 
     IEnumerator MoveTo(Vector2 endPos, float speed) {
+        if (mode == TranslateMode.Teleport && index == points.Length-1) {
+            transform.position = origin+points[index];
+            index = 0;
+
+        } else if (mode == TranslateMode.DoReverse && index == points.Length-1) {
+            index = 0;
+        } else {
+            index++;
+        }
+
+        t.Finished += delegate {t = new Task(MoveTo(points[index], moveSpeed));};
+
+        if (mode == TranslateMode.Teleport && index == 0) yield break;
+        
         float timeElapsed = 0;
-
-        Vector2 startPos = transform.position;
-
-        float actualSpeed = Vector2.Distance(startPos, endPos)/speed;
-
+        Vector2 startPos = (Vector2)transform.position;
+        float actualSpeed = Vector2.Distance(startPos-origin, endPos)/speed;
+    
         while (timeElapsed < actualSpeed) {
             timeElapsed += Time.deltaTime;
             transform.position = Vector2.Lerp(startPos, origin+endPos, timeElapsed/actualSpeed);
